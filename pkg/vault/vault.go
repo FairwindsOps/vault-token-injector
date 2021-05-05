861fbdc50b27080fbd09a871e2f6abe9cdf3265c
Adam Zahorscak <69020096+Azahorscak@users.noreply.github.com>
2021-05-05 15:22:49 -0400

    Add initial vault commands (#4)

---
+++ pkg/vault/vault.go
@@ -0,0 +1,64 @@
+package vault
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// Token represents a token structure in Vault
+type Token struct {
+	Data struct {
+		TTL int `json:"ttl"`
+	} `json:"data"`
+	Auth struct {
+		ClientToken string `json:"client_token"`
+	} `json:"auth"`
+}
+
+// Creates a token using the provided role
+func CreateToken(role string) (*Token, error) {
+	output, _, err := execute("vault", "token", "create", fmt.Sprintf("-role=%s", role), "-format=json")
+	if err != nil {
+		return nil, fmt.Errorf("Error creating token: %s", err.Error())
+	}
+	token := &Token{}
+	err = json.Unmarshal([]byte(output), token)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling vault token: %s", err.Error())
+	}
+	fmt.Println(token)
+	return token, nil
+}
+
+// NeedsRenewal makes sure we have a valid token
+func NeedsRenewal(t Token) (bool, error) {
+	data, _, err := execute("vault", "token", "lookup", "-format=json", t.Auth.ClientToken)
+	if err != nil {
+		return false, fmt.Errorf("error looking up vault token: %s", err.Error())
+	}
+
+	token := &Token{}
+	err = json.Unmarshal(data, token)
+	if err != nil {
+		return false, fmt.Errorf("error unmarshaling vault token: %s", err.Error())
+	}
+
+	//Check the TTL and return true if it needs to be renewed.
+	if token.Data.TTL < 1800 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// execute returns the output and error of a command run using inventory environment variables.
+func execute(name string, arg ...string) ([]byte, string, error) {
+	cmd := exec.Command(name, arg...)
+	data, err := cmd.CombinedOutput()
+	output := strings.TrimSpace(string(data))
+	if err != nil {
+		return nil, "", fmt.Errorf("exit code %d running command %s: %s", cmd.ProcessState.ExitCode(), cmd.String(), output)
+	}
+	return data, output, nil
+}
