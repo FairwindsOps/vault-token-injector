c11aa6fbf2e23a286f1b40017628e8e7399e0510
Luke Reed <luke@fairwinds.com>
2021-05-12 14:32:10 -0400

    add Dockerfile, Makefile, and version subcommand (#9)
    * add Dockerfile, Makefile, and version subcommand
    
    * fix Makefile

--- cmd/root.go
+++ cmd/root.go
@@ -58,7 +58,9 @@ func validateArgs(cmd *cobra.Command, args []string) error {
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(VERSION string, COMMIT string) {
+	version = VERSION
+	versionCommit = COMMIT
 	cobra.CheckErr(rootCmd.Execute())
 }
 
--- cmd/version.go
+++ cmd/version.go
@@ -13,17 +13,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package cmd
 
-import "github.com/fairwindsops/vault-token-injector/cmd"
+import (
+	"fmt"
 
-var (
-	// version is set during build
-	version = "development"
-	// commit is set during build
-	commit = "n/a"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	cmd.Execute(version, commit)
+var (
+	version       string
+	versionCommit string
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the current version of the tool.",
+	Long:  `Prints the current version.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Version:" + version + " Commit:" + versionCommit)
+	},
 }
