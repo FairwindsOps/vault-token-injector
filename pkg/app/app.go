bcb23ca051fb889feca54b897e728447efc98b05
Andrew Suderman <andrew@sudermanjr.com>
2021-09-22 19:06:41 -0600

    use a waitgroup to parallelize. improve logging

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/fairwindsops/vault-token-injector/pkg/vault"
 )
 
+// App is the main application struct
 type App struct {
 	Config         *Config
 	CircleToken    string
@@ -22,15 +24,20 @@ type App struct {
 	TFCloudToken   string
 }
 
-// Config represents the top level our applications config yaml file
+// Config represents the configuration file
 type Config struct {
-	CircleCI             []CircleCIConfig `mapstructure:"circleci"`
-	TFCloud              []TFCloudConfig  `mapstructure:"tfcloud"`
-	VaultAddress         string           `mapstructure:"vault_address"`
-	TokenVariable        string           `mapstructure:"token_variable"`
-	OrphanTokens         bool             `mapstructure:"orphan_tokens"`
-	TokenTTL             time.Duration    `mapstructure:"token_ttl"`
-	TokenRefreshInterval time.Duration    `mapstructure:"token_refresh_interval"`
+	CircleCI []CircleCIConfig `mapstructure:"circleci"`
+	TFCloud  []TFCloudConfig  `mapstructure:"tfcloud"`
+	// The address of the vault server to use when creating tokens
+	VaultAddress string `mapstructure:"vault_address"`
+	// The variable name to use when setting a vault token. Defaults to VAULT_ADDR
+	TokenVariable string `mapstructure:"token_variable"`
+	// If true, all tokens will be created with the orphan flag set to true
+	OrphanTokens bool `mapstructure:"orphan_tokens"`
+	// The TTL of the tokens that will be created. Defaults to 30 minutes
+	TokenTTL time.Duration `mapstructure:"token_ttl"`
+	// The interval at which the token will be refreshed. Defaults to 1 hour
+	TokenRefreshInterval time.Duration `mapstructure:"token_refresh_interval"`
 }
 
 // CircleCIConfig represents a specific instance of a CircleCI project we want to
@@ -44,11 +51,17 @@ type CircleCIConfig struct {
 // TFCloudConfig represents a specific instance of a TFCloud workspace we want to
 // update an environment variable for
 type TFCloudConfig struct {
-	Workspace     string   `mapstructure:"workspace"`
-	VaultRole     *string  `mapstructure:"vault_role"`
+	// Workspace is the ID of the workspace in tfcloud. Should begin with ws- and is required
+	Workspace string `mapstructure:"workspace"`
+	// Name is an optional field that can be used to identify a workspace
+	Name string `mapstructure:"name"`
+	// VaultRole is the vault role to use for the token in this workspace
+	VaultRole *string `mapstructure:"vault_role"`
+	// VaultPolicies is a list of policies that will be given to the token in this workspace
 	VaultPolicies []string `mapstructure:"vault_policies"`
 }
 
+// NewApp creates a new App from the given configuration options
 func NewApp(circleToken, vaultTokenFile, tfCloudToken string, config *Config) *App {
 	app := &App{
 		Config:         config,
@@ -57,11 +70,11 @@ func NewApp(circleToken, vaultTokenFile, tfCloudToken string, config *Config) *A
 		VaultTokenFile: vaultTokenFile,
 	}
 	if len(app.Config.CircleCI) > 0 && circleToken == "" {
-		klog.Warning("CircleCI is configured but no token was provided.")
+		klog.Error("CircleCI is configured but no token was provided.")
 	}
 
 	if len(app.Config.TFCloud) > 0 && tfCloudToken == "" {
-		klog.Warning("TFCloud is configured but no token was provided.")
+		klog.Error("TFCloud is configured but no token was provided.")
 	}
 	if app.Config.TokenVariable == "" {
 		app.Config.TokenVariable = "VAULT_TOKEN"
@@ -89,6 +102,7 @@ func NewApp(circleToken, vaultTokenFile, tfCloudToken string, config *Config) *A
 	return app
 }
 
+// Run starts the application
 func (a *App) Run() error {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -110,56 +124,73 @@ func (a *App) Run() error {
 }
 
 func (a *App) updateCircleCI() {
+	var wg sync.WaitGroup
 	for _, project := range a.Config.CircleCI {
-		projName := project.Name
-		projVariableName := a.Config.TokenVariable
-		token, err := vault.CreateToken(project.VaultRole, project.VaultPolicies, a.Config.TokenTTL, a.Config.OrphanTokens)
-		if err != nil {
-			klog.Error(err)
-			continue
-		}
-		klog.Infof("setting env var %s to vault token value", projVariableName)
-		if err := circleci.UpdateEnvVar(projName, projVariableName, token.Auth.ClientToken, a.CircleToken); err != nil {
-			klog.Error(err)
-			continue
-		}
-		if err := circleci.UpdateEnvVar(projName, "VAULT_ADDR", a.Config.VaultAddress, a.CircleToken); err != nil {
-			klog.Error(err)
-			continue
-		}
+		wg.Add(1)
+		go a.updateCircleCIInstance(project, &wg)
+	}
+	wg.Wait()
+}
+
+func (a *App) updateCircleCIInstance(project CircleCIConfig, wg *sync.WaitGroup) {
+	defer wg.Done()
+	projName := project.Name
+	projVariableName := a.Config.TokenVariable
+	token, err := vault.CreateToken(project.VaultRole, project.VaultPolicies, a.Config.TokenTTL, a.Config.OrphanTokens)
+	if err != nil {
+		klog.Errorf("error making token for %s", projName, err)
+		return
+	}
+	klog.Infof("setting env var %s to vault token value", projVariableName)
+	if err := circleci.UpdateEnvVar(projName, projVariableName, token.Auth.ClientToken, a.CircleToken); err != nil {
+		klog.Errorf("error updating %s with token value: %s", projVariableName, err.Error)
+		return
+	}
+	if err := circleci.UpdateEnvVar(projName, "VAULT_ADDR", a.Config.VaultAddress, a.CircleToken); err != nil {
+		klog.Errorf("error updating VAULT_ADDR in %s: %s", projName, err)
+		return
 	}
 }
 
 func (a *App) updateTFCloud() {
+	var wg sync.WaitGroup
 	for _, instance := range a.Config.TFCloud {
-		token, err := vault.CreateToken(instance.VaultRole, instance.VaultPolicies, a.Config.TokenTTL, a.Config.OrphanTokens)
-		if err != nil {
-			klog.Error(err)
-			continue
-		}
-		klog.Infof("setting env var %s to vault token value", a.Config.TokenVariable)
-		tokenVar := tfcloud.Variable{
-			Key:       a.Config.TokenVariable,
-			Value:     token.Auth.ClientToken,
-			Token:     a.TFCloudToken,
-			Sensitive: true,
-			Workspace: instance.Workspace,
-		}
-		if err := tokenVar.Update(); err != nil {
-			klog.Error(err)
-			continue
-		}
-		addressVar := tfcloud.Variable{
-			Key:       "VAULT_ADDR",
-			Value:     a.Config.VaultAddress,
-			Sensitive: false,
-			Token:     a.TFCloudToken,
-			Workspace: instance.Workspace,
-		}
-		if err := addressVar.Update(); err != nil {
-			klog.Error(err)
-			continue
-		}
+
+		wg.Add(1)
+		go a.updateTFCloudInstance(instance, &wg)
+	}
+	wg.Wait()
+}
+
+func (a *App) updateTFCloudInstance(instance TFCloudConfig, wg *sync.WaitGroup) {
+	defer wg.Done()
+	token, err := vault.CreateToken(instance.VaultRole, instance.VaultPolicies, a.Config.TokenTTL, a.Config.OrphanTokens)
+	if err != nil {
+		klog.Errorf("error creating token for ws %s: %s", instance.Workspace, err.Error())
+		return
+	}
+	klog.Infof("setting env var %s to vault token value", a.Config.TokenVariable)
+	tokenVar := tfcloud.Variable{
+		Key:       a.Config.TokenVariable,
+		Value:     token.Auth.ClientToken,
+		Token:     a.TFCloudToken,
+		Sensitive: true,
+		Workspace: instance.Workspace,
+	}
+	if err := tokenVar.Update(); err != nil {
+		klog.Errorf("error update token for ws %s: %s", instance.Workspace, err.Error())
+		return
+	}
+	addressVar := tfcloud.Variable{
+		Key:       "VAULT_ADDR",
+		Value:     a.Config.VaultAddress,
+		Sensitive: false,
+		Token:     a.TFCloudToken,
+		Workspace: instance.Workspace,
+	}
+	if err := addressVar.Update(); err != nil {
+		klog.Errorf("error update VAULT_ADDR for ws %s: %s", instance.Workspace, err.Error())
+		return
 	}
 }
 
