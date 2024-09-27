3673a5115e66466d520ca0b09985f03e6e5eda49
Andy Suderman <andy@suderman.dev>
2024-09-27 08:23:03 -0600

    feat: log Circle rate limits

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/fairwindsops/vault-token-injector
 
-go 1.20
+go 1.22
 
 require (
 	github.com/hashicorp/go-tfe v1.31.0
--- pkg/circleci/circleci.go
+++ pkg/circleci/circleci.go
@@ -25,8 +25,35 @@ func UpdateEnvVar(projName, env_variable_name, env_variable_value, circleToken s
 	if err != nil {
 		return err
 	}
+
+	if err := handleCircleRateLimit(res); err != nil {
+		return err
+	}
+
 	if res.StatusCode != http.StatusCreated {
+
 		return fmt.Errorf("Failed updating CircleCI. Status Code returned: %d", res.StatusCode)
 	}
+	return nil
+}
+
+// handleCircleRateLimit handles these https://circleci.com/docs/api-developers-guide/#rate-limits
+func handleCircleRateLimit(response *http.Response) error {
+	if response.StatusCode != 429 {
+		return nil
+	}
+	rateLimitHeaders := map[string]string{
+		"RateLimit-Limit":   "",
+		"X-RateLimit-Limit": "",
+		"RateLimit-Reset":   "",
+		"X-RateLimit-Reset": "",
+	}
+
+	for header, value := range rateLimitHeaders {
+		rateLimitHeaders[value] = response.Header.Get(header)
+	}
+
+	klog.Warningf("rate limit encountered %v", rateLimitHeaders)
+
 	return nil
 }
